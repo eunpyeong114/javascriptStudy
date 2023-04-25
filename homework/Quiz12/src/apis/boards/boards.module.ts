4e1d8452c9d2e0356c7042881a54c35a7f39aa19
eunpyeong114 <eunpyeongjeon@gmail.com>
2023-04-25 11:00:40 +0900

    12일차 과제

---
+++ homework/Quiz12/src/apis/boards/boards.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { BoardsResolver } from './boards.resolver';
+import { BoardsService } from './boards.service';
+
+@Module({
+  imports: [],
+  providers: [BoardsService, BoardsResolver],
+})
+export class BoardsModule {}
